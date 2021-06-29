84ad87d4830b60a1f9a6dd59ab75168ba4dfedab
AzamFarooque <azamfarooque69@gmail.com>
2021-06-29 22:20:11 +0530

    Project setup
    This commits include, project setup

---
+++ RandomDogs/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  RandomDogs
+//
+//  Created by Farooque Azam on 29/06/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ RandomDogs/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RandomDogs
+//
+//  Created by Farooque Azam on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
